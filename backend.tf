4033934ae3d0022e089ba9b358cc95f6e2b59cfc
Yash Varma <71536409+yash509@users.noreply.github.com>
2025-05-11 08:30:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "backend88364" # Replace with your actual S3 bucket name
+    bucket = "backend8836" # Replace with your actual S3 bucket name
     key    = "terraform.tfstate"
     region = "ap-south-1" # ap-south-1
   }
