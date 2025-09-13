e836c1e12fc06c46262d6458055771f6e186b7ae
Yash Varma <71536409+yash509@users.noreply.github.com>
2025-09-13 12:02:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,9 +87,9 @@ resource "aws_eks_node_group" "example" {
   subnet_ids      = data.aws_subnets.public.ids
 
   scaling_config {
-    desired_size = 2
-    max_size     = 2
-    min_size     = 1
+    desired_size = 3
+    max_size     = 3
+    min_size     = 2
   }
   instance_types = ["t2.medium"]
 
@@ -102,3 +102,4 @@ resource "aws_eks_node_group" "example" {
   ]
 
 }
+
